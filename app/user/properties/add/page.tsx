88b4fce9bbe2ba82a767289c75fb5023efa8825d
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2026-01-28 06:56:12 +0500

    add property page with Prisma data fetching

---
+++ app/user/properties/add/page.tsx
@@ -0,0 +1,12 @@
+import prisma from "@/lib/prisma";
+import AddPropertyForm from "./_components/AddPropertyForm";
+
+const AddPropertyPage = async () => {
+  const [propertyTypes, propertyStatuses] = await Promise.all([
+    prisma.propertyType.findMany(),
+    prisma.propertyStatus.findMany(),
+  ]);
+  return <AddPropertyForm types={propertyTypes} statuses={propertyStatuses} />;
+};
+
+export default AddPropertyPage;
