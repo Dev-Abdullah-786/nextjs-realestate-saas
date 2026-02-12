278b7ac94e9698aca809f14e29f2711940b707e1
Muhammad Abdullah Khan <abdullahkhanlodhi1458@gmail.com>
2026-02-12 08:19:56 +0500

    implement Stripe CheckoutForm with PaymentElement integration

---
+++ app/user/subscription/_components/CheckoutForm.tsx
@@ -0,0 +1,81 @@
+import { toast } from "react-toastify";
+import { FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+import { SubscriptionPlan } from "@prisma/client";
+import { saveSubscription } from "@/lib/actions/subscription";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { Button, Modal, ModalBody, ModalContent, ModalHeader } from "@nextui-org/react";
+
+interface Props {
+  show: boolean;
+  setShow: (show: boolean) => void;
+  plan: SubscriptionPlan;
+}
+
+const CheckoutForm = (props: Props) => {
+  const [isLoading, setLoading] = useState(false);
+  const stripe = useStripe();
+  const elements = useElements();
+  const { user } = useKindeBrowserClient();
+  const router = useRouter();
+  if (!user) return;
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      if (!elements || !stripe) return;
+
+      const result = await stripe?.confirmPayment({
+        elements,
+        confirmParams: {
+          return_url: "http://localhost:3000/user/profile",
+        },
+        redirect: "if_required",
+      });
+
+      if (result.error) {
+        toast.error(result.error.message);
+      } else {
+        await saveSubscription({
+          paymentId: result.paymentIntent.id,
+          planId: props.plan.id,
+          userId: user?.id,
+        });
+        toast.success("Payment Sucessfull!");
+        router.push("/user/profile");
+      }
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <Modal isOpen={props.show}>
+      <ModalContent>
+        <ModalHeader>Complete your subscription purchase</ModalHeader>
+        <ModalBody>
+          <form onSubmit={handleSubmit}>
+            <PaymentElement />
+            <div className="flex justify-center gap-4">
+              <Button
+                isDisabled={isLoading}
+                onClick={() => props.setShow(false)}
+                color="danger"
+                variant="light"
+              >
+                Cancel
+              </Button>
+              <Button isLoading={isLoading} color="primary" type="submit">
+                Pay
+              </Button>
+            </div>
+          </form>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default CheckoutForm;
